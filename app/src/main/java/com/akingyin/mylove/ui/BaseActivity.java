e8f5b0095e7c760e40f8987d8281561775e9b0b2
akingyin1987 <akingyin@163.com>
2016-05-20 18:10:52 +0800

    写好BaseActivity

--- app/src/main/java/com/akingyin/mylove/ui/BaseActivity.java
+++ app/src/main/java/com/akingyin/mylove/ui/BaseActivity.java
@@ -2,7 +2,6 @@ package com.akingyin.mylove.ui;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import butterknife.ButterKnife;
 import com.akingyin.librarys.base.BaseAppCompatActivity;
