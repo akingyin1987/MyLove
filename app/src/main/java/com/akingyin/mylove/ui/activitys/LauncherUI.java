357615e41c4a5d1d147ffabe755328c9e576d67e
akingyin1987 <akingyin@163.com>
2016-05-24 18:25:43 +0800

    写好BaseActivity

--- app/src/main/java/com/akingyin/mylove/ui/activitys/LauncherUI.java
+++ app/src/main/java/com/akingyin/mylove/ui/activitys/LauncherUI.java
@@ -10,7 +10,10 @@
 
 package com.akingyin.mylove.ui.activitys;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import com.akingyin.mylove.R;
 
 /**
  * @ Description:
@@ -22,5 +25,9 @@ import android.support.v7.app.AppCompatActivity;
  */
 public class LauncherUI extends AppCompatActivity{
 
-
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
 }
